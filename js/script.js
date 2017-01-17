033eeefa8dfd9c619bc6e33ebb7733b8750da1e0
Ganev <a.a.ganev@gmail.com>
2017-01-17 15:50:08 +0300

    some functions moved to utils.js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 $(document).ready(function () {
     var count = generateCount();
-    var input_array = generate(count);
-    $("#input").append("<p id=\"input_txt\">Input array</p>");
-    var input_txt = $("#input_txt");
-    appendArray(input_txt, input_array, "input_array");
+    var data = generateArray(count);
+    var inputTitle = $("<p id=\"input_txt\">Input array</p>");
+    $("#input").append(inputTitle);
+    appendListFromArray(inputTitle, data, "input_array");
 
     $("#generate_btn").click(function () {
         count = generateCount();
         $("#input_array").remove();
-        input_array = generate(count);
-        appendArray(input_txt, input_array, "input_array")
+        data = generateArray(count);
+        appendListFromArray(inputTitle, data, "input_array")
     });
 
     $("#sort_btn").click(function () {
@@ -17,46 +17,14 @@ $(document).ready(function () {
         $("#output").append("<p id=\"output_txt\">Output array</p>");
         var sortingType = $('input[name=sortingType]:checked').val();
         if (sortingType == "BubbleSort") {
-            bubbleSort(input_array);
+            bubbleSort(data);
         }
     })
 });
 
-function generateCount() {
-    var count = Math.floor((Math.random() * 10 + 1));
-    switch (count) {
-        case 0:
-            return count + 3;
-        case 1:
-            return count + 2;
-        case 2:
-            return ++count;
-        default:
-            return count;
-    }
-}
-
-function generate(count) {
-    var array = [count];
-    for (var i = 0; i < count; i++) {
-        array[i] = (Math.floor((Math.random() * 100) + 1))
-    }
-    return array;
-}
-
-function appendArray(element, array, array_id) {
-    var input = "<ul id=" + array_id + "></ul>";
-    $(element).append(input);
-    for (var i = 0; i < array.length; i++) {
-        var li = $("<li>" + array[i] + "</li>");
-        $("#" + array_id).append(li);
-    }
-    $(element).hide().fadeIn(500);
-}
-
 function printOutputArray(array) {
     $("#output_array").remove();
-    appendArray($("#output_txt"), array, "output_array");
+    appendListFromArray($("#output_txt"), array, "output_array");
 }
 
 function bubbleSort(array) {
---
+++ js/utils.js
@@ -0,0 +1,30 @@
+function generateCount() {
+    var count = Math.floor((Math.random() * 10 + 1));
+    switch (count) {
+        case 0:
+            return count + 3;
+        case 1:
+            return count + 2;
+        case 2:
+            return ++count;
+        default:
+            return count;
+    }
+}
+
+function generateArray(count) {
+    var array = [count];
+    for (var i = 0; i < count; i++) {
+        array[i] = (Math.floor((Math.random() * 100) + 1))
+    }
+    return array;
+}
+
+function appendListFromArray(element, array, array_id) {
+    var input = $("<ul id=" + array_id + "></ul>");
+    $(element).append(input);
+    for (var i = 0; i < array.length; i++) {
+        var li = $("<li>" + array[i] + "</li>");
+        li.hide().appendTo($("#" + array_id)).show("slow");
+    }
+}
