4d6f0e67c681207bfd07f1e8e0fff6ed08a8205b
Ganev <a.a.ganev@gmail.com>
2017-01-20 10:14:58 +0300

    add tooltips  comments and some animation stuff

--- js/script.js
+++ js/script.js
@@ -1,33 +1,64 @@
 $(document).ready(function () {
+    //data initialization
     var count = generateCount();
     var data = generateArray(count);
     var inputTitle = $("<p id=\"input_txt\">Input array:</p>");
+    var generateBtn = $("#generate_btn");
+    var sortBtn = $("#sort_btn");
+    var buttons = $(".buttons");
 
+    //input array render
     $("#input").append(inputTitle);
     appendListFromArray(inputTitle, data, "input_array");
 
-    $("#generate_btn").click(function () {
+    //"Generate" button click handling
+    generateBtn.click(function () {
         count = generateCount();
         $("#input_array").remove();
         data = generateArray(count);
         appendListFromArray(inputTitle, data, "input_array")
     });
 
+    //"Sort" button click handling
     var outputTitleAdded = false;
-
-    $("#sort_btn").click(function () {
+    sortBtn.click(function () {
         var outputTitle = $("<p id=\"output_txt\">Sorting:</p>");
         if (!outputTitleAdded) {
             $("#output").append(outputTitle);
             outputTitleAdded = true;
         }
-        var sortingType = $('input[name=sortingType]:checked').val();
-        if (sortingType === "BubbleSort") {
-            bubbleSort($("#output_txt"), data, "output_array");
-        }
-    })
+        runSelectedSort(data)
+    });
+
+    //View tooltips for buttons
+    drawTooltip(buttons, generateBtn, "Generate new sorting data");
+    drawTooltip(buttons, sortBtn, "Sort generated data");
 });
 
+/**
+ * Run function corresponding to selection sort type
+ *
+ * @param data - array to sort
+ */
+function runSelectedSort(data) {
+    var sortingType = $('input[name=sortingType]:checked').val();
+    var outputTxt = $("#output_txt");
+    if (sortingType === "BubbleSort") {
+        outputTxt.show();
+        bubbleSort(outputTxt, data, "output_array");
+    } else {
+        outputTxt.hide();
+        drawMessage($("#input_array"), 0, "Sorry, tree sort is not implemented yet.")
+    }
+}
+
+/**
+ * Bubble sort function
+ *
+ * @param element - element for adding container to render data
+ * @param data - array to sort
+ * @param arrayName - array id
+ */
 function bubbleSort(element, data, arrayName) {
     var array = data.slice();
     var timeout = 0;
@@ -54,4 +85,6 @@ function bubbleSort(element, data, arrayName) {
     }
     timeout += 500;
     redrawWithTimeout(element, array, arrayName, undefined, timeout);
+    timeout += 500;
+    drawMessage(element, timeout, "Sorting successfully finished!");
 }
--- js/utils.js
+++ js/utils.js
@@ -1,3 +1,10 @@
+//This file contains utility functions
+
+/**
+ * Generate random number
+ *
+ * @returns {number} greater then 3 or equal
+ */
 function generateCount() {
     var count = Math.floor((Math.random() * 10 + 1));
     switch (count) {
@@ -12,6 +19,12 @@ function generateCount() {
     }
 }
 
+/**
+ * Generate array filled with random numbers
+ *
+ * @param count - array size
+ * @returns {[*]} generated array
+ */
 function generateArray(count) {
     var array = [count];
     for (var i = 0; i < count; i++) {
@@ -20,42 +33,107 @@ function generateArray(count) {
     return array;
 }
 
+/**
+ * Sorted data render
+ *
+ * @param element - element for adding container to render data
+ * @param array - data to render
+ * @param arrayId - new element id
+ */
 function appendListFromArray(element, array, arrayId) {
+    appendListFromArrayCustom(element, array, arrayId, undefined)
+}
+
+/**
+ * Sorted data render with custom style for compared array elements
+ *
+ * @param element - element for adding container to render data
+ * @param array - data to render
+ * @param arrayId - new element id
+ * @param index - array index for customization
+ */
+function appendListFromArrayCustom(element, array, arrayId, index) {
     var input = $("<ul id=" + arrayId + "></ul>");
     $(element).append(input);
     for (var i = 0; i < array.length; i++) {
         var li = $("<li>" + array[i] + "</li>");
-        li.appendTo($("#" + arrayId));
-    }
-}
-
-function appendListFromArrayCustom(element, array, arrayId, current) {
-    var input = $("<ul id=" + arrayId + "></ul>");
-    $(element).append(input);
-    for (var i = 0; i < array.length; i++) {
-        var li = $("<li>" + array[i] + "</li>");
-        if (i === current) {
-            li.css("background-color", "#ca8833");
-        }
-        if (i === (current + 1)) {
-            li.css("background-color", "#ca8833");
+        if (index !== undefined) {
+            if (i === index) {
+                li.css("background-color", "#ca8833");
+            }
+            if (i === (index + 1)) {
+                li.css("background-color", "#ca8833");
+            }
         }
         li.appendTo($("#" + arrayId));
     }
 }
 
-function redraw(element, array, arrayName, index) {
-    $("#" + arrayName).remove();
-    if (index === undefined) {
-        appendListFromArray(element, array, arrayName);
-    } else {
-        appendListFromArrayCustom(element, array, arrayName, index);
-    }
+/**
+ * Redraw data
+ *
+ * @param element - element for adding container to render data
+ * @param array - data to render
+ * @param arrayId - new element id
+ * @param index - array index for customization
+ */
+function redraw(element, array, arrayId, index) {
+    $("#" + arrayId).remove();
+    appendListFromArrayCustom(element, array, arrayId, index);
 }
 
-function redrawWithTimeout(element, data, arrayName, j, timeout) {
+/**
+ * Redraw data with timeout
+ *
+ * @param element - element for adding container to render data
+ * @param data - data to render
+ * @param arrayId - new element id
+ * @param index - array index for customization
+ * @param timeout - timeout
+ */
+function redrawWithTimeout(element, data, arrayId, index, timeout) {
     var array = data.slice();
     setTimeout(function () {
-        redraw(element, array, arrayName, j);
+        redraw(element, array, arrayId, index);
     }, timeout);
+}
+
+/**
+ * Draw tooltip
+ *
+ * @param appendTo - element for adding container of tooltip
+ * @param element - element for display tooltip
+ * @param text - tooltip text
+ */
+function drawTooltip(appendTo, element, text) {
+    element.mouseenter(function () {
+        var tooltip = $("<div id=\"tooltip\">" + text + "</div>");
+        appendTo.append(tooltip);
+        tooltip.css({left: 50, top: -70})
+            .animate({opacity: 1}, 100);
+        setTimeout(function () {
+            tooltip.fadeOut(1000);
+        }, 1000);
+    }).mouseleave(function () {
+        $("#tooltip").remove();
+    })
+}
+
+/**
+ * Draw message with timeout
+ *
+ * @param element - element for adding container of message
+ * @param timeout - drawing timeout
+ * @param msg - message text
+ */
+function drawMessage(element, timeout, msg) {
+    var finish = $("<p>" + msg + "</p>");
+    setTimeout(function () {
+        finish.hide();
+        element.append(finish);
+        finish.fadeIn(1000).fadeOut(3000);
+    }, timeout);
+    setTimeout(function () {
+        finish.remove();
+    }, timeout + 3500)
 }
